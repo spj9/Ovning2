d2b1b747e38e84809a45a1883b639f77a4878327
spj9 <sanny_j@hotmail.com>
2024-05-23 17:08:24 +0200

    Commit calculate group price.

--- Ovning2/Program.cs
+++ Ovning2/Program.cs
@@ -3,63 +3,101 @@
 // Skriv ut huvudmenyn med de olika valen
 while (true)
 {
-    Console.WriteLine("\nVälkommen! Här på huvudmenyn navigerar du genom att välja en siffra i menyn nedan:");
+    Console.WriteLine("Välkommen! \nHär på huvudmenyn navigerar du genom att välja en siffra i menyn nedan:");
     Console.WriteLine("0. Avsluta");
-    Console.WriteLine("1. Vad kostar det att gå på bio");
+    Console.WriteLine("1. Biobiljett - pris per person");
+    Console.WriteLine("2. Biobiljett - pris för ett sällskap ");
 
     string? input = Console.ReadLine();
 
-        // Checka vilket val som gjorts
-        switch (input)
-        {
-            case "0":
-                Environment.Exit(0);
-                break;
-            case "1":
-                CheckTicketPrice("Vänligen ange ålder på besökaren.");
-                break;
-            default:
-                PrintInvalidInput(null);
-                break;
-        }
+    // Checka vilket val som gjorts
+    switch (input)
+    {
+        case "0":
+            Environment.Exit(0);
+            break;
+        case "1":
+            CheckTicketPrice(true, "Vänligen ange ålder på besökaren:");
+            break;
+        case "2":
+            // CheckGroupPrice();
+            break;
+        default:
+            PrintInvalidInput(null);
+            break;
+    }
 }
 // Skriv ut biljettpriset för det val som gjorts eller anropa metod för felaktig input.
-int CheckTicketPrice(string query)
+int CheckTicketPrice(Boolean check, string query)
 {
     while (true)
     {
         Console.WriteLine(query);
         string? input = Console.ReadLine();
-            if (int.TryParse(input, out int age))
+        if (int.TryParse(input, out int age))
+        {
+            if (age < 0)
             {
-                if (age < 0)
-                {
-                    PrintInvalidInput("Du kan inte ange ett negativt tal för ålder.");
-                    continue;
-                }
-                if (age < 20)
-                {
+                PrintInvalidInput("Du kan inte ange ett negativt tal för ålder.");
+                continue;
+            }
+            if (age < 20)
+            {
+                if (check) {
                     Console.WriteLine("Biljettpris, ungdom: 80 kr");
-                    return 80;
                 }
-                if (age < 65)
-                {
-                    Console.WriteLine("Biljettpris, vuxen: 120 kr");
-                    return 120;
-                }
-                if (age  > 64)
-                {
-                    Console.WriteLine("Biljettpris, pensionär: 90 kr");
-                    return 90;
-                }
+                return 80;
             }
-            else
+            if (age < 65)
             {
-                PrintInvalidInput("You need to enter a positive integer!");
+                if (check) {
+                    Console.WriteLine("Biljettpris, vuxen: 120 kr");
+                }
+                return 120;
             }
+            if (age > 64)
+            {
+                if (check) {
+                    Console.WriteLine("Biljettpris, pensionär: 90 kr");
+                }
+                return 90;
+            }
+        }
+        else
+        {
+            PrintInvalidInput("You need to enter a positive integer!");
+        }
+    }
+}
+// Skriv ut biljettpriset för en vald grupp.
+void CheckGroupPrice()
+{
+    int groupPrice = 0;
+    while (true)
+    {
+        Console.WriteLine("Ange antal besökare i gruppen:");
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out int groupSize))
+        {
+            if (groupSize < 1)
+            {
+                PrintInvalidInput("Vänligen ange ett tal högre än 0.");
+                continue;
+            }
+
+            for (int i = 0; i < groupSize; i++)
+            {
+                groupPrice += CheckTicketPrice(false, $"Ange ålder för besökare {i + 1}");
+            }
+        }
+
+        Console.WriteLine($"Number of people in the group: {groupSize}");
+        Console.WriteLine($"Cost for the whole group: {groupPrice}");
+        break;
     }
 }
 
+// Skriv ut felmeddelande.
 void PrintInvalidInput(string? errorDescription)
 {
     Console.WriteLine($"FEL: {errorDescription ?? ""} Vänligen försök igen!\n");
